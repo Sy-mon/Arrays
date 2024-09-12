3ab5687f44dbfddde1ea3965c62e7682d0b97865
sy-mon <simonmuyp@gmail.com>
2024-09-12 10:58:05 -0400

    strings update

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String[] students = {"Peter", "Simon", "Jon", "Luna"};
+        String[] grades = {"A", "C", "D", "F", "B"};
+        int[] registrationNumber = {20, 1, 45, 56};
+
+        System.out.println(students[0]);
+        System.out.println(grades[0]);
+        System.out.println(registrationNumber[1]);
+
+    }
+}
